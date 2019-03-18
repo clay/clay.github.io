9d266429babdfa545649da37a9c50aa41eb2067a
james-owen <james.owen@nymag.com>
2019-03-18 15:53:58 -0400

    added CNAME

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -93,6 +93,8 @@ const siteConfig = {
   // You may provide arbitrary config keys to be used as needed by your
   // template. For example, if you need your repo's URL...
   //   repoUrl: 'https://github.com/facebook/test-site',
+
+  cname: 'docs.clayplatform.com'
 };
 
 module.exports = siteConfig;
